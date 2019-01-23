061e7c9634611ae8f04b3cf861945886f340c59a
Digamber <digamber@positronx.io>
2019-01-23 11:41:26 +0530

    Updated enviorment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAJ1N3mU7eLG18MjLjFvAlJb8jVbN0ai0I",
-    authDomain: "angularfirebase-auth.firebaseapp.com",
-    databaseURL: "https://angularfirebase-auth.firebaseio.com",
-    projectId: "angularfirebase-auth",
-    storageBucket: "angularfirebase-auth.appspot.com",
-    messagingSenderId: "101924368184"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   }
 };
